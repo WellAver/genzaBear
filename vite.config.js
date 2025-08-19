3e9e13b112878c8b9dd1e175415564ae45c50022
WellAver <vitalyavery@gmail.com>
2025-08-20 01:11:39 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/WellAver/',               
+  base: '/genzaBear/',               
   build: {
     outDir: 'dist',
     assetsDir: '',
