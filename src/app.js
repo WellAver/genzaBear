b6b6c370075ba120a03a83103d96789477bbd6cd
WellAver <vitalyavery@gmail.com>
2025-08-20 01:14:08 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const BASE = (import.meta?.env?.BASE_URL) ?? '/';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
